6199fcee43847a7efc3add132eede0e6e4e7ba19
Mursyied Qathafi <38604611+mursyiedqathafi@users.noreply.github.com>
2022-03-24 03:55:24 +0700

    Create Struktur Program Python - Part 1.py

---
+++ Python for Data Professional Beginner/Python for Data Professional Beginner - Part 1/Struktur Program Python - Part 1.py
@@ -0,0 +1,9 @@
+print ("Belajar Python menyenangkan")
+print ("Halo Dunia")
+print ("Hello World!")
+
+bilangan1 = 5
+bilangan2 = 10
+kalimat1 = "Belajar Bahasa Python"
+
+print(bilangan1 + bilangan2)
